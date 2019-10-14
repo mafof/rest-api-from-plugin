2411e118ab86f035e0f571a49385c69429e28f16
root <root@ovz1.matclass.pk7kn.vps.myjino.ru>
2019-10-14 09:47:18 +0000

    upd cron

--- cron.php
+++ cron.php
@@ -1,5 +1,11 @@
 <?php
+file_put_contents('log.txt', 'test\t\n', FILE_APPEND | LOCK_EX);
+define('IS_CRON', 1);
+require_once "index.php";
+/*
 if($argc == 2 && $argv[1] == '11223322112233') {
+	file_put_contents('log.txt', 'test\t\n', FILE_APPEND | LOCK_EX);
     define('IS_CRON', 1);
     require_once "index.php";
 }
+*/
